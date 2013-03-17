cdf96eeb5f68b4ad6a689cdf8412e720b510ef6c
Gabriel Montagné Láscaris-Comneno <gabriel@rojored.com>
2013-03-17 05:31:55 +0000

    initial line info.

--- ask.py
+++ ask.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+STOP = "@@ stop @@"
+SEPARATOR = "@@ ============"
+
 class Asker:
-    def ask(self, q):
-        print("ask", q['q'])
+    def ask(self, question, hint = ''):
+
+        if "n" in question:
+            line_info = "## %(n)s:%(ln)s" % question
+        else: 
+            line_info = ""
+
+        print("line_info", line_info)
+
+        return "hh"
+
+class QuestionAbort(Exception):
+    pass
+
+class AbortAndReload(Exception):
+    pass
