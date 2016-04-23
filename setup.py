09511380746c94042196a7c2fd400f46b227ec3b
gabriel montagné láscaris-comneno <gabriel@rojored.com>
2016-04-23 13:07:21 +0000

    stub version and README.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='lung',
+    author='Gabriel Montagné Láscaris-Comneno',
+    author_email='gabriel@tibas.london',
+    license='MIT',
+    version='0.0.1'
+)
