ededb8013c9ef549feae2041aa7168fcfba142bd
gabriel montagné láscaris-comneno <gabriel@tibas.london>
2023-10-28 13:10:21 +0200

    add defaults to help

--- lung/__main__.py
+++ lung/__main__.py
@@ -21,7 +21,7 @@ def main():
 
     logging.info('\n\nstart lung run')
 
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-s", action='store_true',
                         help='sequential (non random)', required=False)
     parser.add_argument('-f', nargs='+', required=False, help='files')
